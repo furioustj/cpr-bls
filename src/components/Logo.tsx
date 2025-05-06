5b7598ebf993e18c3b2529464405c046d4e65ee6
furioustj <furioustejash1@gmail.com>
2025-05-07 00:37:53 +0530

    LogoRescue

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -25,7 +25,7 @@ const Logo: React.FC<LogoProps> = ({
       whileTap={{ scale: 0.95 }}
     >
       <img
-        src="src\components\LogoRescue.png"
+        src="https://rescuefederation.com/wp-content/uploads/2024/11/cropped-Untitled-design.png"
         alt="Rescue Federation Logo"
         className={`w-full h-full object-contain ${variant === 'light' ? 'brightness-100' : 'brightness-90'}`}
       />
